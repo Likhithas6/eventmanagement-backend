5f270dc305fd1eb071f3ee37eab2fd78f31a5b23
Likhithas6 <likhithareddy063@gmail.com>
2025-09-22 19:31:22 +0530

    Update ManiProjectApplication.java

--- src/main/java/com/klef/fsd/sdp/ManiProjectApplication.java
+++ src/main/java/com/klef/fsd/sdp/ManiProjectApplication.java
@@ -8,14 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class ManiProjectApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ManiProjectApplication.class, args);
-        System.out.println("backend is Running Successfully !!...");
-    }
-
-    // Required for WAR deployment in external Tomcat
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ManiProjectApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManiProjectApplication.class, args);
+        System.out.println("Mani Project is running successfully!");
     }
 }
